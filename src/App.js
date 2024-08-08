0ccf0c40861e38ae6845d2d438e01e5bfa91410c
Erfan <erfuun.mi82@gmail.com>
2024-08-08 13:42:35 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+return(
+  <ColorModeContext.Provider value={colorMode}>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+
+
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+)
+
 }
 
 export default App;
